fa232388208ab57221a07bf67e5607204d0b093f
Nathan Kluth <nlkluth@gmail.com>
2017-10-13 16:18:21 -0600

    settings view wip

--- pages/Settings.js
+++ pages/Settings.js
@@ -1,8 +1,23 @@
 // @flow
 
-import React from "react";
-import { Text } from "react-native";
+import React from 'react';
+import { Text, Slider, View, StyleSheet } from 'react-native';
 
-const Settings = () => <Text>Settings</Text>;
+const Settings = () => (
+  <View style={styles.container}>
+    <View style={styles.information}>
+      <Text>Sample output:</Text>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  information: {
+    alignItems: 'center'
+  }
+});
 
 export default Settings;
